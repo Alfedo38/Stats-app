b4d44df4f00115b1c0802f9b0fc73d2cf48e4b71
Alfedo <macsy38@gmail.com>
2026-03-13 18:28:35 -0300

    Compatibilidad de URL para Vercel build

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,5 @@
 import { defineConfig } from '@prisma/config';
-import dotenv from 'dotenv';
-
-// Esto obliga a Prisma a leer tu archivo .env
-dotenv.config();
 
 export default defineConfig({
   earlyAccess: true,
-  datasource: {
-    url: process.env.DATABASE_URL,
-  },
 });
